7ea56e192ae68716c5285728b8ca06f494284efc
bamsak <114239407+bamsak@users.noreply.github.com>
2023-08-07 13:52:58 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.swift
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    let vowels: [Character] = ["a", "e", "i", "o", "u"]
-    var result: String = ""
-    for char in my_string {
-        if !vowels.contains(char) {
-            result.append(char)
-        }
-    }
-    return result
+    return my_string.filter{!["a", "e", "i", "o","u"].contains($0)}
 }
