35d9702743010197e1100dcf8445615ac40d99b3
bamsak <114239407+bamsak@users.noreply.github.com>
2023-09-28 23:05:14 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.swift
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.swift
@@ -1,12 +1,9 @@
 import Foundation
 
 func solution(_ array:[Int]) -> [Int] {
-    var result: [Int] = []
     guard let number = array.sorted().last else {return []}
-    guard let index: Int = array.firstIndex(where: {$0 == number}) else {return []}
-    result.append(number)
-    result.append(index)
+    guard let index: Int = array.firstIndex(of: number) else {return []}
     
-    return result
+    return [number, index]
 
 }
