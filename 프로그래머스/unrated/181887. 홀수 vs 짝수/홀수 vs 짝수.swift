c1a8bf6baeb77076229e5128d0154ce6bafadf76
bamsak <114239407+bamsak@users.noreply.github.com>
2023-10-16 00:20:17 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.swift
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.swift
@@ -1,8 +1,7 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
-    let totalIndex = num_list.count - 1
-    let odd = (0...totalIndex).map{$0 % 2 == 0 ? num_list[$0] : 0}.reduce(0, +) 
-    let even = (0...totalIndex).map{$0 % 2 != 0 ? num_list[$0] : 0}.reduce(0, +) 
+    let odd = (0..<num_list.count).map{$0 % 2 == 0 ? num_list[$0] : 0}.reduce(0, +) 
+    let even = (0..<num_list.count).map{$0 % 2 != 0 ? num_list[$0] : 0}.reduce(0, +) 
     return odd > even ? odd : even
 }
