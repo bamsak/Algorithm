cc4f45d536065af99e6ecac1e82da93137930334
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-01 13:38:14 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 80100 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.swift
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -1,28 +1,33 @@
-let n = Int(readLine()!)!
-let aNumbers = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-let m = Int(readLine()!)!
-let mArr = readLine()!.split(separator: " ").map { Int($0)! }
-
-
-for number in mArr {
-    var left = 0
-    var right = n - 1
+func foo() {
+    let n = Int(readLine()!)!
+    let aNumbers = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+    let m = Int(readLine()!)!
+    let mArr = readLine()!.split(separator: " ").map { Int($0)! }
+    var result = ""
     
-    var check = false
-    
-    while left <= right {
-        let mid = (left + right) / 2
+    for number in mArr {
+        var left = 0
+        var right = n - 1
         
-        if number < aNumbers[mid] {
-            right = mid - 1
-        } else {
-            left = mid + 1
-        }
+        var check = false
         
-        if number == aNumbers[mid] {
-            check = true
-            break
+        while left <= right {
+            let mid = (left + right) / 2
+            
+            if number < aNumbers[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+            
+            if number == aNumbers[mid] {
+                check = true
+                break
+            }
         }
+        result += check ? "1\n" : "0\n"
+        
     }
-    print("\(check ? 1 : 0)")
+    print(result)
 }
+foo()
