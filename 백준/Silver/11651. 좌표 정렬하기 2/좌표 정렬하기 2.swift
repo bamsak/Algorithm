62778c4ca2cfbb20932113b7706c559757a02bea
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-02 19:15:52 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 192 ms, Memory: 75328 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.swift
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.swift
@@ -1,15 +1,17 @@
 var arr = [(x: Int, y: Int)]()
+var result = ""
 
 for _ in 0..<Int(readLine()!)! {
     let xy = readLine()!.split(separator: " ").map { Int($0)! }
     arr.append((xy[0], xy[1]))
 }
 
-let result = arr.sorted {
+let sortedArr = arr.sorted {
     if $0.y == $1.y {
         return $0.x < $1.x
     }
     return $0.y < $1.y
 }
 
-result.forEach { print("\($0.x) \($0.y)") }
+sortedArr.forEach { result.append("\($0.x) \($0.y)\n") }
+print(result)
