a0f808b28dd568b951fe80ace9e145696d4d9491
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-09 19:10:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 144 ms, Memory: 77160 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.swift
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.swift
@@ -1,11 +1,12 @@
 let n = Int(readLine()!)!
 var users = [(age: Int, name: String)]()
-
+var result = ""
 for _ in 0..<n {
     let user = readLine()!.split(separator: " ")
     users.append((Int(user[0])!, String(user[1])))
 }
 
 users.sorted { $0.age < $1.age }.forEach {
-    print("\($0.age) \($0.name)")
+    result += "\($0.age) \($0.name) \n"
 }
+print(result)
