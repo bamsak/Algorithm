6c856308ad3726d54d8ba5d27fcadca9e488570b
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-11 23:05:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.swift
@@ -0,0 +1,14 @@
+var n = Int(readLine()!)!
+var result = 0
+
+while n > 0 {
+    if n % 5 == 0 {
+        result += n / 5
+        n = 0
+        break
+    }
+    n -= 3
+    result += 1
+}
+
+n == 0 ? print(result) : print(-1)
