c2f1d183e907cd71013e48f2cede53c29a1a1e40
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-12 22:52:17 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.06 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int {
+    let a = A.sorted()
+    let b = B.sorted(by: >)
+    var result = 0
+    
+    for i in 0..<a.count {
+        result += a[i] * b[i]
+    }
+    
+    return result
+}
