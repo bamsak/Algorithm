004c13b5c4fc27c4013794868bb4bee039bdb19f
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-22 19:53:55 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.swift
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.swift
@@ -1,12 +1,8 @@
-var arr = [[Int]](repeating: [Int](repeating: 0, count: 14), count: 15)
+var arr = [[Int]](repeating: [Int](repeating: 1, count: 14), count: 15)
 arr[0] = Array(1...14)
 
 for i in 1..<arr.count {
-    for j in 0..<arr[i].count {
-        if j == 0 {
-            arr[i][j] = 1
-            continue
-        }
+    for j in 1..<arr[i].count {
         arr[i][j] = arr[i][j - 1] + arr[i - 1][j]
     }
 }
