a6cb7b3341c459d17ea85c68ba68d28fd6c7fca4
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-26 13:46:48 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 384 ms, Memory: 83132 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.swift
@@ -0,0 +1,34 @@
+let nmr = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m, r) = (nmr[0], nmr[1], nmr[2])
+
+var edges = [[Int]](repeating: [], count: n + 1)
+var visited = [Bool](repeating: false, count: n + 1)
+
+for _ in 0..<m {
+    let uv = readLine()!.split(separator: " ").map { Int($0)! }
+    edges[uv[0]].append(uv[1])
+    edges[uv[1]].append(uv[0])
+}
+
+for i in 1...n {
+    edges[i] = edges[i].sorted()
+}
+
+var count = 0
+var result = [Int](repeating: 0, count: n + 1)
+var stack = [r]
+
+while stack.isEmpty == false {
+    let vertex = stack.removeLast()
+    if visited[vertex] == true { continue }
+    visited[vertex] = true
+    count += 1
+    result[vertex] = count
+    
+    for edge in edges[vertex] {
+        if visited[edge] == false {
+            stack.append(edge)
+        }
+    }
+}
+print(result[1...].map { String($0) }.joined(separator: "\n"))
