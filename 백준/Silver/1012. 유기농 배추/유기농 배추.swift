05d901661a763f5c8d03eb2fa23541b7e6c924de
bamsak <114239407+bamsak@users.noreply.github.com>
2024-02-29 15:51:59 +0900

    [Silver II] Title: 유기농 배추, Time: 16 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.swift
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.swift
@@ -3,7 +3,6 @@ for _ in 0..<Int(readLine()!)! {
     let (m, n, k) = (mnk[0], mnk[1], mnk[2])
     
     var field = Array(repeating: Array(repeating: 0, count: m), count: n)
-
     
     for _ in 0..<k {
         let xy = readLine()!.split(separator: " ").map { Int($0)! }
@@ -14,27 +13,29 @@ for _ in 0..<Int(readLine()!)! {
     let dy = [0, -1, 0, 1]
     
     var count = 0
+    var stack = [(x: Int, y: Int)]()
     
     for y in 0..<n {
         for x in 0..<m {
             if field[y][x] == 1 {
-                dfs(x: x, y: y)
+                stack.append((x, y))
+                field[y][x] = 0
+                while stack.isEmpty == false {
+                    let coord = stack.removeLast()
+                    for i in 0..<dx.count {
+                        let newX = coord.x + dx[i]
+                        let newY = coord.y + dy[i]
+                        if newX < 0 || newY < 0 || newX >= m || newY >= n { continue }
+                        if field[newY][newX] != 1 { continue }
+                        
+                        field[newY][newX] = 0
+                        stack.append((newX, newY))
+                    }
+                }
                 count += 1
             }
         }
     }
-    
-    func dfs(x: Int, y: Int){
-        field[y][x] = 0
-        for i in 0..<dx.count {
-            let newX = x + dx[i]
-            let newY = y + dy[i]
-            
-            if newX < 0 || newY < 0 || newX >= m || newY >= n { continue }
-            if field[newY][newX] != 1 { continue }
-            
-            dfs(x: newX, y: newY)
-        }
-    }
+
     print(count)
 }
