af9f5352a5c1e46c0359865a144610fb32bb0bb2
bamsak <114239407+bamsak@users.noreply.github.com>
2024-03-05 19:08:10 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 72 ms, Memory: 81216 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.swift
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.swift
@@ -1,44 +1,21 @@
-struct Queue<T> {
-    private var input = [T]()
-    private var output = [T]()
-    
-    var isEmpty: Bool {
-        input.isEmpty && output.isEmpty
-    }
-    
-    mutating func enqueue(_ newElement: T) {
-        input.append(newElement)
-    }
-    
-    mutating func dequeue() -> T {
-        if output.isEmpty {
-            output = input.reversed()
-            input = [T]()
-        }
-        return output.removeLast()
-    }
-}
 let nm = readLine()!.split(separator: " ").map { Int($0)! }
 let (n, m) = (nm[0], nm[1])
 
 var campus = Array(repeating: Array(repeating: "", count: 0), count: n)
 var visited = Array(repeating: Array(repeating: false, count: m), count: n)
+
 for i in 0..<n {
     let input = readLine()!.map { String($0) }
     campus[i] = input
 }
 
+var stack = [(x: Int, y: Int)]()
 
-var check = false
-var queue = Queue<(x: Int, y: Int)>()
-
-for x in 0..<m {
-    if check { break }
-    for y in 0..<n {
+for y in 0..<n {
+    for x in 0..<m {
         if campus[y][x] == "I" {
-            queue.enqueue((x, y))
+            stack.append((x, y))
             visited[y][x] = true
-            check = true
             break
         }
     }
@@ -49,16 +26,17 @@ let my = [1, 0, -1, 0]
 
 var count = 0
 
-while queue.isEmpty == false {
-    let coord = queue.dequeue()
+while stack.isEmpty == false {
+    let coord = stack.removeLast()
     
     for i in 0..<mx.count {
         let newX = coord.x + mx[i]
         let newY = coord.y + my[i]
         
-        if newX < 0 || newY < 0 || newX >= m || newY >= n {
+        if newX < 0 || newX >= m || newY < 0 || newY >= n {
             continue
         }
+        
         if visited[newY][newX] { continue }
         
         if campus[newY][newX] == "X" {
@@ -70,9 +48,8 @@ while queue.isEmpty == false {
             count += 1
         }
         
-        queue.enqueue((newX, newY))
         visited[newY][newX] = true
+        stack.append((newX, newY))
     }
 }
-
-print(count == 0 ? "TT" : count)
+print(count == 0 ? "TT": count)
