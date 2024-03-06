5a9f418f6f32ac1cf5ce08c1e5e6051837c575ad
bamsak <114239407+bamsak@users.noreply.github.com>
2024-03-06 13:35:35 +0900

    [Silver II] Title: 최대 힙, Time: 72 ms, Memory: 70672 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.swift
@@ -0,0 +1,78 @@
+struct Heap<T> {
+    private var array = [T]()
+    private let compare: (T, T) -> Bool
+    
+    init(compare: @escaping (T, T) -> Bool) {
+        self.compare = compare
+    }
+    
+    mutating func enheap(_ newElement: T) {
+        array.append(newElement)
+        
+        var childIndex = array.count - 1
+        var parentIndex = (childIndex - 1) / 2
+        
+        while childIndex > 0 && compare(array[childIndex], array[parentIndex]) {
+            array.swapAt(childIndex, parentIndex)
+            childIndex = parentIndex
+            parentIndex = (childIndex - 1) / 2
+        }
+    }
+    
+    mutating func deheap() -> T? {
+        if array.isEmpty {
+            return nil
+        }
+        
+        if array.count == 1 {
+            return array.removeLast()
+        }
+        
+        let value = array[0]
+        array[0] = array.removeLast()
+        
+        var index = 0
+        
+        while true {
+            let leftChildIndex = index * 2 + 1
+            let righChildIndex = leftChildIndex + 1
+            var target = index
+            
+            if leftChildIndex < array.count && compare(array[leftChildIndex], array[target]) {
+                target = leftChildIndex
+            }
+            
+            if righChildIndex < array.count && compare(array[righChildIndex], array[target]) {
+                target = righChildIndex
+            }
+            
+            if target == index {
+                break
+            }
+            
+            array.swapAt(target, index)
+            index = target
+        }
+        return value
+    }
+}
+
+var heap = Heap<Int>(compare: >)
+var result = ""
+for _ in 0..<Int(readLine()!)! {
+    let num = Int(readLine()!)!
+    
+    if num != 0 {
+        heap.enheap(num)
+        continue
+    }
+    
+    guard let value = heap.deheap() else {
+        result += "0\n"
+        continue
+    }
+    
+    result += "\(value)\n"
+}
+
+print(result)
