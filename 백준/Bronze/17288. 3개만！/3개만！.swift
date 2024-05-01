686df18bc5c710defe2633b99acc9d90b1e2fbd5
bamsak <114239407+bamsak@users.noreply.github.com>
2024-05-01 22:28:08 +0900

    [Bronze I] Title: 3개만!, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/17288. 3개만！/3개만！.swift
+++ 백준/Bronze/17288. 3개만！/3개만！.swift
@@ -5,12 +5,12 @@ var arr = [input[0]]
 for i in 1..<input.count {
     if input[i - 1] == (input[i] - 1) {
         arr.append(input[i])
-    } else {
-        if arr.count == 3 {
-            result += 1
-        }
-        arr = [input[i]]
+        continue
     }
+    if arr.count == 3 {
+        result += 1
+    }
+    arr = [input[i]]
 }
 
 if arr.count == 3 {
