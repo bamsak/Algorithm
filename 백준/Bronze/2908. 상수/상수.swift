5f06a86567bc407b04ba620146ac45cdef26fd81
bamsak <114239407+bamsak@users.noreply.github.com>
2024-05-18 17:37:45 +0900

    [Bronze II] Title: 상수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.swift
+++ 백준/Bronze/2908. 상수/상수.swift
@@ -1,4 +1,1 @@
-let ab = readLine()!.split(separator: " ")
-let a = Int(ab[0].map { Int(String($0))! }.reversed().map { String($0) }.joined())!
-let b = Int(ab[1].map { Int(String($0))! }.reversed().map { String($0) }.joined())!
-a > b ? print(a) : print(b)
+print(readLine()!.split(separator: " ").map { Int(String($0.reversed()))!}.max()!)
