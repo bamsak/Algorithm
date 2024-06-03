b1dbbff166e9b2c84eff5686b9d7a108720ba839
bamsak <114239407+bamsak@users.noreply.github.com>
2024-06-03 18:45:18 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 468 ms, Memory: 95596 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.swift
@@ -0,0 +1,32 @@
+let nmr = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m, r) = (nmr[0], nmr[1], nmr[2])
+
+var edges = Array(repeating: Array(repeating: 0, count: 0), count: n + 1)
+var visited = Array(repeating: false, count: n + 1)
+
+for _ in 0..<m {
+    let uv = readLine()!.split(separator: " ").map { Int($0)! }
+    let (u, v) = (uv[0], uv[1])
+    edges[u].append(v)
+    edges[v].append(u)
+}
+
+var stack = [(node: Int, depth: Int)]()
+var result = Array(repeating: -1, count: n + 1)
+result[r] = 0
+
+func dfs(_ node: Int, _ depth: Int) {
+    visited[node] = true
+    result[node] = depth
+    
+    for edge in edges[node].sorted(by: >) where !visited[edge] {
+        visited[edge] = true
+        dfs(edge, depth + 1)
+    }
+}
+
+dfs(r, 0)
+
+result[1...].forEach {
+    print($0)
+}
