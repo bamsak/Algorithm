713523b20af78b1d27e43a480c71cf836644c559
bamsak <114239407+bamsak@users.noreply.github.com>
2024-06-24 19:58:59 +0900

    [Silver I] Title: 보석 상자, Time: 256 ms, Memory: 76792 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.swift
@@ -0,0 +1,29 @@
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m) = (nm[0], nm[1])
+var jewels = [Int]()
+
+for _ in 0..<m {
+    jewels.append(Int(readLine()!)!)
+}
+jewels.sort()
+
+var left = 1
+var right = jewels.last!
+
+while left <= right {
+    let mid = (left + right) / 2
+    let count = jewels.map {
+        if $0 % mid != 0 {
+            return $0 / mid + 1
+        }
+        return $0 / mid
+    }.reduce(0, +)
+    
+    if count <= n {
+        right = mid - 1
+    } else {
+        left = mid + 1
+    }
+}
+
+print(left)
