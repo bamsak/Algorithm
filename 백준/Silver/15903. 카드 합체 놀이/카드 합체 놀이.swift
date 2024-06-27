1cd8ba1ce27d4bb24bfdb1e855a94796a34f6a38
bamsak <114239407+bamsak@users.noreply.github.com>
2024-06-27 12:02:40 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 16 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.swift
@@ -0,0 +1,73 @@
+struct Heap<T> {
+    private var array = [T]()
+    private let compare: (T, T) -> Bool
+    
+    init(compare: @escaping (T, T) -> Bool) {
+        self.compare = compare
+    }
+    
+    mutating func enheap(_ newElement: T) {
+        var childIndex = array.count
+        var parentIndex = (childIndex - 1) / 2
+        
+        array.append(newElement)
+        
+        while childIndex > 0 && compare(array[childIndex], array[parentIndex]) {
+            array.swapAt(childIndex, parentIndex)
+            childIndex = parentIndex
+            parentIndex = (childIndex - 1) / 2
+        }
+    }
+    
+    mutating func deheap() -> T? {
+        if array.isEmpty { return nil }
+        if array.count == 1 { return array.removeLast() }
+        
+        let value = array[0]
+        array[0] = array.removeLast()
+        var index = 0
+        
+        while true {
+            let leftChildIndex = index * 2 + 1
+            let rightChildIndex = leftChildIndex + 1
+            var target = index
+            
+            if leftChildIndex < array.count && compare(array[leftChildIndex], array[target]) {
+                target = leftChildIndex
+            }
+            
+            if rightChildIndex < array.count && compare(array[rightChildIndex], array[target]) {
+                target = rightChildIndex
+            }
+            
+            if target == index { break }
+            array.swapAt(target, index)
+            index = target
+        }
+        
+        return value
+    }
+}
+
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m) = (nm[0], nm[1])
+let cards = readLine()!.split(separator: " ").map { Int($0)! }
+var heap = Heap<Int> { $0 < $1 }
+cards.forEach { heap.enheap($0) }
+
+var result = 0
+
+for _ in 0..<m {
+    guard let x = heap.deheap() else { break }
+    guard let y = heap.deheap() else { break }
+    let new = x + y
+    heap.enheap(new)
+    heap.enheap(new)
+}
+
+while true {
+    guard let value = heap.deheap() else { break }
+    result += value
+}
+
+print(result)
