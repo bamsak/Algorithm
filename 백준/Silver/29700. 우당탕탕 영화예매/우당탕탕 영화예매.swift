f19940a88b500682696e00526e43672383d033b1
bamsak <114239407+bamsak@users.noreply.github.com>
2024-08-12 19:46:42 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 288 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.swift
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.swift
@@ -1,17 +1,13 @@
 let nmk = readLine()!.split(separator: " ").map { Int($0)! }
 let (n, m, k) = (nmk[0], nmk[1], nmk[2])
-var cinema = Array(repeating: Array(repeating: 0, count: 0), count: n)
-
-for i in 0..<n {
-    cinema[i] = readLine()!.map { Int(String($0))!}
-}
-
 var result = 0
 
-for y in 0..<n {
+for _ in 0..<n {
+    let input =  readLine()!.map { Int(String($0))! }
     var current = 0
+    
     for x in 0..<m {
-        if cinema[y][x] == 0 {
+        if input[x] == 0 {
             current += 1
         } else {
             current = 0
