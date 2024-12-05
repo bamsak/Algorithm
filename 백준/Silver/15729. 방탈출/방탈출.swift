c0ddd5ad6462bdcb6b14600b6a06622ad123a76c
bamsak <114239407+bamsak@users.noreply.github.com>
2024-12-05 14:41:13 +0900

    [Silver II] Title: 방탈출, Time: 188 ms, Memory: 122368 KB -BaekjoonHub

--- 백준/Silver/15729. 방탈출/방탈출.swift
+++ 백준/Silver/15729. 방탈출/방탈출.swift
@@ -1,27 +1,18 @@
 let n = Int(readLine()!)!
-let arr = readLine()!.split(separator: " ").map { Int($0)! }
-var temp = Array(repeating: 0, count: n)
-var index = 0
-
+let arr = readLine()!.split(separator: " ").map { Int($0)! == 1 ? true : false }
+var temp = Array(repeating: false, count: n)
 var result = 0
 
-while index < n {
-    if arr[index] == temp[index] {
-        index += 1
-        continue
-    }
+for i in 0..<n where arr[i] != temp[i] {
     result += 1
-    
-    if index + 2 < n {
-        for i in index...index + 2 {
-            temp[i] = temp[i] == 1 ? 0 : 1
+    if i + 2 < n {
+        for  j in i...i + 2 {
+            temp[j].toggle()
         }
     } else {
-        for i in index..<arr.count {
-            temp[i] = temp[i] == 1 ? 0 : 1
+        for j in i..<n {
+            temp[j].toggle()
         }
     }
-    index += 1
 }
-
 print(result)
