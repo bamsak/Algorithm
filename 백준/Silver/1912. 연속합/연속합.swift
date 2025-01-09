e5787b21fb3dd00bc2001c2590757e24aff80cfb
bamsak <114239407+bamsak@users.noreply.github.com>
2025-01-09 16:53:35 +0900

    [Silver II] Title: 연속합, Time: 28 ms, Memory: 76116 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.swift
@@ -0,0 +1,10 @@
+let n = Int(readLine()!)!
+let nums = readLine()!.split(separator: " ").map { Int($0)! }
+var dp = [Int](repeating: Int.min, count: n)
+dp[0] = nums[0]
+
+for i in 1..<n {
+    dp[i] = max(nums[i], nums[i] + dp[i - 1])
+}
+
+print(dp.max()!)
