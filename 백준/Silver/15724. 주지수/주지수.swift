df68c0eba342be0fb6c9859d66c818b0088b0bd8
bamsak <114239407+bamsak@users.noreply.github.com>
2025-01-29 14:35:59 +0900

    [Silver I] Title: 주지수, Time: 384 ms, Memory: 87620 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.swift
@@ -0,0 +1,26 @@
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m) = (nm[0], nm[1])
+var board = [[Int]]()
+for _ in 0..<n {
+    board.append(readLine()!.split(separator: " ").map { Int($0)! })
+}
+
+var psum = Array(repeating: Array(repeating: 0, count: m + 1), count: n + 1)
+
+for y in 1...n {
+    for x in 1...m {
+        psum[y][x] = board[y - 1][x - 1] + psum[y - 1][x] + psum[y][x - 1] - psum[y - 1][x - 1]
+    }
+}
+
+let k = Int(readLine()!)!
+var result = ""
+
+for _ in 0..<k {
+    let xy = readLine()!.split(separator: " ").map { Int($0)! }
+    let (x1, y1, x2, y2) = (xy[1], xy[0], xy[3], xy[2])
+    
+    result += "\(psum[y2][x2] - psum[y1 - 1][x2] - psum[y2][x1 - 1] + psum[y1 - 1][x1 - 1])\n"
+}
+
+print(result)
