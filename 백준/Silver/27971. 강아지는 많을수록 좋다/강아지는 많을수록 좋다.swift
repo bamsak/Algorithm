b576dc3b03e179f5a54b8d602611fa0c1182a48e
bamsak <114239407+bamsak@users.noreply.github.com>
2025-02-24 22:36:42 +0900

    [Silver I] Title: 강아지는 많을수록 좋다, Time: 32 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.swift
+++ 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.swift
@@ -1,5 +1,23 @@
+struct Queue<T> {
+    private var input = [T]()
+    private var output = [T]()
+    
+    mutating func enqueue(_ newElement: T) {
+        input.append(newElement)
+    }
+    
+    mutating func dequeue() -> T? {
+        if output.isEmpty {
+            output = input.reversed()
+            input = []
+        }
+        return output.popLast()
+    }
+}
+
 let nmab = readLine()!.split(separator: " ").map { Int($0)! }
 let (n, m, a, b) = (nmab[0], nmab[1], nmab[2], nmab[3])
+
 
 var temp = [Bool](repeating: false, count: n + 1)
 
@@ -12,19 +30,29 @@ for _ in 0..<m {
     }
 }
 
-var dp = [Int](repeating: Int.max, count: n + 1)
-dp[0] = 0
+var result = -1
+var queue = Queue<(dog: Int, count: Int)>()
 
-for i in 0...n where dp[i] != Int.max {
-    let (newA, newB) = (i + a, i + b)
+queue.enqueue((0, 0))
+
+while let current = queue.dequeue() {
+    if current.dog == n {
+        result = current.count
+        break
+    }
+    
+    let newA = current.dog + a
+    let newB = current.dog + b
     
     if newA <= n && !temp[newA] {
-        dp[newA] = min(dp[newA], dp[i] + 1)
+        temp[newA] = true
+        queue.enqueue((newA, current.count + 1))
     }
     
     if newB <= n && !temp[newB] {
-        dp[newB] = min(dp[newB], dp[i] + 1)
+        temp[newB] = true
+        queue.enqueue((newB, current.count + 1))
     }
 }
 
-print(dp[n] == Int.max ? -1 : dp[n])
+print(result)
