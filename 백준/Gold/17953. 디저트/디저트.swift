502061e3d30461ad8cc6323697604e1ea73ad989
bamsak <114239407+bamsak@users.noreply.github.com>
2025-03-01 16:46:18 +0900

    [Gold V] Title: 디저트, Time: 464 ms, Memory: 85416 KB -BaekjoonHub

--- 백준/Gold/17953. 디저트/디저트.swift
+++ 백준/Gold/17953. 디저트/디저트.swift
@@ -8,9 +8,9 @@ for _ in 0..<m {
 
 var dp = Array(repeating: Array(repeating: 0, count: n), count: m)
 var result = 0
+
 for i in 0..<m {
     dp[i][0] = desserts[i][0]
-    result = max(dp[i][0], result)
 }
 
 for i in 1..<n {
@@ -18,9 +18,11 @@ for i in 1..<n {
         for k in 0..<m {
             let new = j == k ? desserts[j][i] / 2 : desserts[j][i]
             dp[j][i] = max(dp[j][i], dp[k][i - 1] + new)
-            result = max(dp[j][i], result)
         }
     }
 }
 
+for i in 0..<m {
+    result = max(result, dp[i][n - 1])
+}
 print(result)
