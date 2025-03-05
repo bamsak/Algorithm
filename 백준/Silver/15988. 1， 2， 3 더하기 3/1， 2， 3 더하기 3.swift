5a5bdc4f21d8b9b2ab01c2399feb692a936afe9f
bamsak <114239407+bamsak@users.noreply.github.com>
2025-03-05 22:30:51 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 36 ms, Memory: 76916 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.swift
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.swift
@@ -1,15 +1,13 @@
+var dp = [Int](repeating: 0, count: 1_000_001)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in 4...1_000_000 {
+    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009
+}
+
 for _ in 0..<Int(readLine()!)! {
     let n = Int(readLine()!)!
-    var dp = [Int](repeating: 0, count: 1_000_001)
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 4
-    
-    if n >= 4 {
-        for i in 4...n {
-            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009
-        }
-    }
-    
     print(dp[n])
 }
