cd454e7d36a31a7712c30d9688a05d2010c6298e
bamsak <114239407+bamsak@users.noreply.github.com>
2025-05-18 23:16:06 +0900

    [Bronze III] Title: 전자레인지, Time: 8 ms, Memory: 69100 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.swift
+++ 백준/Bronze/10162. 전자레인지/전자레인지.swift
@@ -1,24 +1,12 @@
-var (a, b, c) = (0, 0, 0)
-
 var time = Int(readLine()!)!
 
-while time > 0 {
-    if time >= 300 {
-        a += 1
-        time -= 300
-    }
-    
-    if time >= 60 {
-        b += 1
-        time -= 60
-    }
-    
-    if time >= 10 {
-        c += 1
-        time -= 10
-    } else {
-        break
-    }
-}
+var result = ""
+result += "\(time / 300) "
+time %= 300
+result += "\(time / 60) "
+time %= 60
+result += "\(time / 10)"
+time %= 10
 
-print(time == 0 ? "\(a) \(b) \(c)" : -1)
+print(time == 0 ? result : -1)
+
